22e28fe0e8c7da571398de5696509bd349ffd4b5
renanguerra <rebarbosa07@hotmail.com>
2019-10-15 20:45:07 -0300

    adicionado sistema de pontução

--- game.py
+++ game.py
@@ -2,12 +2,14 @@ import time
 
 perguntas = open('perguntas.txt','r')
 respostas = open('respostas.txt','r')
+pontu = open('pontuação.txt','r')
 respostas_linha = respostas.readline()
 
 score = 0
 a = 0
 estado = True      
 
+#-------------------------------FUNÇÕES------------------------------
 def pergunta():
     for linhas in perguntas:
         if linhas == '\n':
@@ -35,9 +37,91 @@ def resposta():
         print('Fim de jogo!')
         print('Tente novamente')
         print('Seu score foi de {} pontos'.format(score))
-            
-                
-nome = input('Qual o seu nome?: ')
+        
+def pontuação():
+    global score
+    global nome
+    pontu = open('pontuação.txt','r')
+
+    scorestr = str(score)
+    pontos = nome + ' ' + scorestr + '\n'
+
+    cabeçalho = pontu.readline()
+    primeiro = pontu.readline()
+    segundo = pontu.readline()
+    terceiro = pontu.readline()
+    quarto = pontu.readline()
+    quinto = pontu.readline()
+    pontu.close()
+
+    primeiro_separa = primeiro.split()
+    primeiro_pontu = int(primeiro_separa[1])
+
+    segundo_separa = segundo.split()
+    segundo_pontu = int(segundo_separa[1])
+
+    terceiro_separa = terceiro.split()
+    terceiro_pontu = int(terceiro_separa[1])
+
+    quarto_separa = quarto.split()
+    quarto_pontu = int(quarto_separa[1])
+
+    quinto_separa = quinto.split()
+    quinto_pontu = int(quinto_separa[1])
+
+
+    if score < quinto_pontu:
+        print('nada acontece')
+    elif score >= primeiro_pontu:
+        pontu = open('pontuação.txt','w')
+        pontu.write(cabeçalho)
+        pontu.write(pontos)
+        pontu.write(primeiro)
+        pontu.write(segundo)
+        pontu.write(terceiro)
+        pontu.write(quarto)
+        pontu.close()
+    elif score >= segundo_pontu and score < primeiro_pontu:
+        pontu = open('pontuação.txt','w')
+        pontu.write(cabeçalho)
+        pontu.write(primeiro)
+        pontu.write(pontos)
+        pontu.write(segundo)
+        pontu.write(terceiro)
+        pontu.write(quarto)
+        pontu.close()
+    elif score >= terceiro_pontu and score < segundo_pontu:
+        pontu = open('pontuação.txt','w')
+        pontu.write(cabeçalho)
+        pontu.write(primeiro)
+        pontu.write(segundo)
+        pontu.write(pontos)
+        pontu.write(terceiro)
+        pontu.write(quarto)
+        pontu.close()
+    elif score >= quarto_pontu and score < terceiro_pontu:
+        pontu = open('pontuação.txt','w')
+        pontu.write(cabeçalho)
+        pontu.write(primeiro)
+        pontu.write(segundo)
+        pontu.write(terceiro)
+        pontu.write(pontos)
+        pontu.write(quarto)
+        pontu.close()
+    elif score >= quinto_pontu and score < quarto_pontu:
+        pontu = open('pontuação.txt','w')
+        pontu.write(cabeçalho)
+        pontu.write(primeiro)
+        pontu.write(segundo)
+        pontu.write(terceiro)
+        pontu.write(quarto)
+        pontu.write(pontos)
+        pontu.close()          
+#------------------------------MAIN------------------------------------------
+
+
+
+nome = input('Digite seu nome: ')
 time.sleep(1)
 print ('\nOlá, {}'.format(nome))
 time.sleep(1)
@@ -47,3 +131,8 @@ time.sleep(1)
 while estado == True:
     resposta_final = pergunta()
     resposta()
+
+pontuação()
+
+for linha in pontu:
+    print(linha)
